86097e6ddaa99ad8968715893b759b7cd271ed60
Adrian Hoitan <adrian.hoitan@ricardo.ch>
2017-08-18 11:52:59 +0200

    Initial : add LaunchDarkly binding + sample

---
+++ Ricardo.LaunchDarkly.Android/Ricardo.LaunchDarkly.Android.Sample/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Com.Launchdarkly.Android;
+
+namespace Ricardo.LaunchDarkly.Android.Sample
+{
+    [Activity(Label = "Ricardo.LaunchDarkly.Android.Sample", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            LDConfig ldConfig = new LDConfig.Builder().SetMobileKey("YOUR_MOBILE_KEY").Build();
+			LDUser user = new LDUser.Builder("user key").Email("fake@example.com").Build();
+
+			// NOTE: This method blocks for up to 5 seconds. See Javadoc or http://docs.launchdarkly.com/docs/android-sdk-reference
+			// for nonblocking options.
+            LDClient ldClient = LDClient.Init(Application, ldConfig, user, 5);
+        }
+    }
+}
+
